34772a48829d1dfd196d9cf2aaad48c274b0963a
kodxana <romanrex19@int.pl>
2023-01-25 15:27:28 +0100

    Update pix2pix.sh

--- pix2pix.sh
+++ pix2pix.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-sed -i 's/demo.launch(debug=True, width="80%", height=2000)/demo.launch(debug=True, width="80%", height=2000, share=True)/' /app/app.py
+pip install triton==2.0.0.dev20221120
+pip install Pillow==9.1.0
+sed -i 's/demo.launch(debug=True, width="80%", height=2000)/demo.launch(debug=True, share=True, server_port=7862)/' /app/app.py
 python /app/app.py
