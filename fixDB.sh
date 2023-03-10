e2497680a408da3e2b8f045f24c73c31fd62bbd0
kodxana <romanrex19@int.pl>
2023-03-10 11:14:12 +0100

    Update fixDB.sh

--- fixDB.sh
+++ fixDB.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+# Check if the directory exists, and if it doesn't, create it
+if [ ! -d "/workspace/sd/stable-diffusion-webui/extensions" ]; then
+  mkdir -p /workspace/sd/stable-diffusion-webui/extensions
+fi
+
 # Clone the repository
-git clone https://github.com/d8ahazard/sd_dreambooth_extension /workspace/sd/stable-diffusion-webui/extensions/sd_dreambooth_extension
+cd /workspace/sd/stable-diffusion-webui/extensions
+git clone https://github.com/d8ahazard/sd_dreambooth_extension
 
 # Install pip requirements
 cd /workspace/sd/stable-diffusion-webui/extensions/sd_dreambooth_extension
+pip install --upgrade pip
 pip install -r requirements.txt
