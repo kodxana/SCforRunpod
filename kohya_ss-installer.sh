d77bff4298a294a61a4e23b6cf34ba24f2e98496
kodxana <romanrex19@int.pl>
2023-03-15 12:15:04 +0100

    Update kohya_ss-installer.sh

--- kohya_ss-installer.sh
+++ kohya_ss-installer.sh
@@ -6,33 +6,28 @@ desktop_path="$HOME/Desktop"
 # Check for Python 3.10 and install if not found
 if ! command -v python3.10 &> /dev/null; then
     echo "Python 3.10 not found. Installing Python 3.10..."
-    apt update
-    apt install -y python3.10 python3.10-venv
+    sudo apt update || { echo "Error updating package repository. Please check your internet connection and try again."; exit 1; }
+    sudo apt install -y python3.10 python3.10-venv || { echo "Error installing Python 3.10. Please check your permissions and try again."; exit 1; }
 fi
 
 # Update the package repository
-apt update
+sudo apt update || { echo "Error updating package repository. Please check your internet connection and try again."; exit 1; }
 
 # Install required packages
-apt install -y python3-tk firefox git
+sudo apt install -y python3-tk firefox git || { echo "Error installing required packages. Please check your permissions and try again."; exit 1; }
 
 # Clone the repository
-git clone https://github.com/bmaltais/kohya_ss.git "$desktop_path/kohya_ss"
+git clone https://github.com/bmaltais/kohya_ss.git "$desktop_path/kohya_ss" || { echo "Error cloning the repository. Please check your internet connection and try again."; exit 1; }
 
-# Check for successful cloning and change directory
-if [ ! -d "$desktop_path/kohya_ss" ]; then
-    echo "Error cloning the repository. Please check your internet connection and try again."
-    exit 1
-fi
-
-cd "$desktop_path/kohya_ss"
+# Change directory
+cd "$desktop_path/kohya_ss" || { echo "Error changing directory. Please check your permissions and try again."; exit 1; }
 
 # Install dependencies
-python3.10 -m venv venv
+python3.10 -m venv venv || { echo "Error creating virtual environment. Please check your permissions and try again."; exit 1; }
 source venv/bin/activate
-pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
-pip install xformers
-pip install --use-pep517 --upgrade -r requirements.txt
+pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116 || { echo "Error installing PyTorch packages. Please check your internet connection and try again."; exit 1; }
+pip install xformers || { echo "Error installing xformers package. Please check your internet connection and try again."; exit 1; }
+pip install --use-pep517 --upgrade -r requirements.txt || { echo "Error installing requirements from the requirements.txt file. Please check your internet connection and try again."; exit 1; }
 
 # Create a launcher script for the app
 launcher_script="$desktop_path/kohya_launcher.sh"
@@ -40,7 +35,7 @@ echo "#!/bin/bash" > $launcher_script
 echo "cd \"$desktop_path/kohya_ss\"" >> $launcher_script
 echo "source venv/bin/activate" >> $launcher_script
 echo "python3 kohya_gui.py" >> $launcher_script
-chmod +x $launcher_script
+chmod +x $launcher_script || { echo "Error making the launcher script executable. Please check your permissions and try again."; exit 1; }
 
 # Print a message to the user indicating the installation is complete
 echo "Kohya SS has been installed. To launch the app, run the kohya_launcher.sh script on your Desktop."
