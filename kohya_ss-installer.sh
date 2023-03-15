38618ce7769177762999b570189565487721a8d5
kodxana <romanrex19@int.pl>
2023-03-15 11:42:50 +0100

    Update kohya_ss-installer.sh

--- kohya_ss-installer.sh
+++ kohya_ss-installer.sh
@@ -1,37 +1,49 @@
 #!/bin/bash
 
+# Get the current desktop path
+desktop_path="$HOME/Desktop"
+
+# Check for Python 3.8 and install if not found
+if ! command -v python3.8 &> /dev/null; then
+    echo "Python 3.8 not found. Installing Python 3.8..."
+    apt update
+    apt install -y python3.8
+fi
+
 # Update the package repository
-apt update
+sudo apt update
 
 # Install required packages
-apt install -y python3-tk python3.8-venv firefox
+sudo apt install -y python3-tk python3.8-venv firefox git
 
 # Clone the repository
-git clone https://github.com/bmaltais/kohya_ss.git
+git clone https://github.com/bmaltais/kohya_ss.git "$desktop_path/kohya_ss"
 
-# Change ownership of the kohya_ss directory to kasm-user
-chown -R kasm-user:kasm-user kohya_ss
+# Check for successful cloning and change directory
+if [ ! -d "$desktop_path/kohya_ss" ]; then
+    echo "Error cloning the repository. Please check your internet connection and try again."
+    exit 1
+fi
+
+cd "$desktop_path/kohya_ss"
 
 # Install dependencies
-cd kohya_ss
-python -m venv venv
+python3.8 -m venv venv
 source venv/bin/activate
 pip install torch==1.12.1+cu116 torchvision==0.13.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
 pip install xformers
 pip install --use-pep517 --upgrade -r requirements.txt
 
 # Create a launcher script for the app
-echo "#!/bin/bash" > ../kohya_launcher.sh
-echo "cd $(pwd)" >> ../kohya_launcher.sh
-echo "source venv/bin/activate" >> ../kohya_launcher.sh
-echo "python3 kohya_gui.py" >> ../kohya_launcher.sh
-chmod +x ../kohya_launcher.sh
-
-# Change ownership of the launcher script to kasm-user
-chown kasm-user:kasm-user ../kohya_launcher.sh
+launcher_script="$desktop_path/kohya_launcher.sh"
+echo "#!/bin/bash" > $launcher_script
+echo "cd \"$desktop_path/kohya_ss\"" >> $launcher_script
+echo "source venv/bin/activate" >> $launcher_script
+echo "python3 kohya_gui.py" >> $launcher_script
+chmod +x $launcher_script
 
 # Print a message to the user indicating the installation is complete
-echo "Kohya SS has been installed. To launch the app, run the kohya_launcher.sh script in the directory above kohya_ss."
+echo "Kohya SS has been installed. To launch the app, run the kohya_launcher.sh script on your Desktop."
 
 # Exit the script
 exit 0
